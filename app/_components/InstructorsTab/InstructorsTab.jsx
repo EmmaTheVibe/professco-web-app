f48579773b91259e3caff38c3c184c996c95a20b
EmmaTheVibe <peteremmanuel316@gmail.com>
2025-08-10 06:58:43 +0100

    updates

--- app/_components/InstructorsTab/InstructorsTab.jsx
+++ app/_components/InstructorsTab/InstructorsTab.jsx
@@ -8,19 +8,114 @@ export default function InstructorsTab() {
           <p>View All</p>
         </button>
       </div>
-      <div className={styles.instructor}>
-        <img
-          src="/images/instructor-avatar.svg"
-          alt="avatar"
-          className={styles.avatar}
-        />
-        <div className={styles.instructorBrief}>
-          <p className={`${styles.instructorName} boldFont`}>Okoro James</p>
-          <p className={styles.instructorTxt}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            varius enim
-          </p>
-        </div>
+      <div className={styles.instructorsGrid}>
+        {[...Array(2)].map((_, index) => (
+          <div className={styles.instructorWrapper} key={index}>
+            <div className={styles.instructor}>
+              <img
+                src="/images/instructor-avatar.svg"
+                alt="avatar"
+                className={styles.avatar}
+              />
+              <div className={styles.instructorBrief}>
+                <p className={`${styles.instructorName} boldFont`}>
+                  Okoro James
+                </p>
+                <p className={styles.instructorTxt}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim
+                </p>
+                <div className={styles.courseTypes}>
+                  <div className={styles.courseType}>
+                    <p className="semiboldFont">CIMA</p>
+                  </div>
+                  <div className={styles.courseType}>
+                    <p className="semiboldFont">ICAN</p>
+                  </div>
+                  <div className={styles.courseType}>
+                    <p className="semiboldFont">ACCA</p>
+                  </div>
+                </div>
+                <div className={styles.instructorSummary}>
+                  <p className={styles.learners}>
+                    6757 <span>learners</span>
+                  </p>
+                  <div className={styles.divider}></div>
+                  <p className={styles.courses}>
+                    3 <span>courses</span>
+                  </p>
+                </div>
+              </div>
+              <div className={styles.dividerB}></div>
+              <div className={styles.gridWrapper}>
+                <div className={styles.cardGrid}>
+                  {[...Array(3)].map((_, index) => (
+                    <div className={styles.card} key={index}>
+                      <img
+                        src="/images/course-banner-sm.png"
+                        alt="banner"
+                        className={styles.banner}
+                      />
+                      <div className={styles.cardInfo}>
+                        <p className={`${styles.cardTitle} boldFont`}>
+                          BA1 Fundamentals of Business Economics
+                        </p>
+                        <p className={`${styles.cardType}`}>ICAN</p>
+                        <div className={styles.rating}>
+                          <img
+                            src="/images/blackstar.svg"
+                            alt="star"
+                            className={styles.blackstar}
+                          />
+                          <p style={{ fontSize: "14px", lineHeight: "20px" }}>
+                            4.8 (150 reviews)
+                          </p>
+                        </div>
+                        <p className={styles.cardTxt}>
+                          <span>What you'll learn: </span>Macroeconomic...
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <p className={styles.moreInfo}>
+              Tim Buchalka
+              <br />
+              Java Python Android and C# Expert Developer - 1.21M students
+              <br />
+              Tim's been a professional software developer for close to 40
+              years. During his career, he has worked for major companies such
+              as Fujitsu, Mitsubishi, and Saab.
+              <br />
+              His video courses are used to train developers in major companies
+              such as Mercedes-Benz, Paypal, VW, Pitney Bowes, IBM, and T-Mobile
+              just to name a few (via the Udemy for Business program).
+              <br />
+              What makes Tim unique is his professional programming career -
+              many instructors have never programmed professionally, let alone
+              had a distinguished professional development career like Tim.
+              <br />
+              Tim has trained over 1.21 million students how to program, way
+              more than a typical IT Professor at a college does in a lifetime.
+              <br />
+              In fact, Tim's courses are often purchased by students struggling
+              to get through college programming courses.
+              <br />
+              "I am learning a lot about Java very quickly. I wish my college
+              courses worked this way, they drag the same amount of material out
+              over months." - Thomas Neal
+              <br />
+              "I love this guy. I'm in school for java right now at a local
+              college and I bought this course hoping it would help clarify the
+              fuzzy areas of my coursework. There's no comparison. Every time I
+              get lost in my textbook, I watch a couple more of these videos and
+              I'm right back on track. He explains everything so perfectly. It
+              sinks right in." - Kristen Andreani
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
