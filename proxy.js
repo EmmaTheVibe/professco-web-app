40842da333ee396bc30b6ffa8beb73e04343b7a1
EmmaTheVibe <peteremmanuel316@gmail.com>
2026-07-07 05:25:42 +0100

    middleware to proxy rename

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 const protectedRoutes = ["/student", "/personalize"];
 const guestOnlyRoutes = ["/login", "/signup"];
 
-export function middleware(request) {
+export function proxy(request) {
   const token = request.cookies.get("token")?.value;
   const { pathname } = request.nextUrl;
 
